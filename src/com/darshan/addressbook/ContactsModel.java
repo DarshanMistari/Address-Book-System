b008ba236734060986c23a636cafc568b7a1f3d7
DarshanMistari <mistaridarshan2@gmail.com>
2024-03-21 19:37:18 +0530

    [MoDIFIED]:Darshan - UC-1 Create Cotacts Model class

---
+++ src/com/darshan/addressbook/ContactsModel.java
@@ -0,0 +1,13 @@
+package com.darshan.addressbook;
+
+public class ContactsModel {
+    private String fName;
+    private String lName;
+    private String address;
+    private String city;
+    private String state;
+    private int zipCode;
+    private String phoneNum;
+    private String email;
+
+}
