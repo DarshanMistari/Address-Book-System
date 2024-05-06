8ec60153add1d9e1d2d794cda2511c066ba76c8b
DarshanMistari <mistaridarshan2@gmail.com>
2024-05-06 12:50:32 +0530

    [MODIFIED]: Darshan - UC-2 Add Contact in Address Book

--- src/com/darshan/addressbook/AddressBookMain.java
+++ src/com/darshan/addressbook/AddressBookMain.java
@@ -1,9 +1,55 @@
 package com.darshan.addressbook;
 
+import java.util.Scanner;
 public class AddressBookMain {
+    Scanner scanner = new Scanner(System.in);
+    ContactsModule contactsModule = new ContactsModule();
+
+    public void createContacts()
+    {
+        System.out.println("Enter the First Name:");
+        contactsModule.setfName(scanner.next());
+
+        System.out.println("Enter the Last Name:");
+        contactsModule.setlName(scanner.next());
+
+        System.out.println("Enter the Address :");
+        contactsModule.setAddress(scanner.next());
+
+        System.out.println("Enter the City:");
+        contactsModule.setCity(scanner.next());
+
+        System.out.println("Enter the State :");
+        contactsModule.setState(scanner.next());
+
+        System.out.println("Enter the ZipCode :");
+        contactsModule.setZipCode(scanner.next());
+
+        System.out.println("Enter the Phone Number :");
+        contactsModule.setPhoneNo(scanner.next());
+
+        System.out.println("Enter the Email Id :");
+        contactsModule.setEmail(scanner.next());
+    }
+
+    public void displayContacts()
+    {
+        System.out.println(" First Name :"+contactsModule.getfName());
+        System.out.println("Last Name :"+contactsModule.getlName());
+        System.out.println("Address :"+contactsModule.getAddress());
+        System.out.println("City :"+contactsModule.getCity());
+        System.out.println("State :"+contactsModule.getState());
+        System.out.println("ZipCode :"+contactsModule.getZipCode());
+        System.out.println("Phone Number :"+contactsModule.getPhoneNo());
+        System.out.println("Email id :"+contactsModule.getEmail());
+    }
 
     public static void main(String []args)
     {
+        AddressBookMain addressBookMain = new AddressBookMain();
+
         System.out.println("Welcome to Address Book Developed by Darshan.");
+        addressBookMain.createContacts();
+        addressBookMain.displayContacts();
     }
 }
--- src/com/darshan/addressbook/ContactsModule.java
+++ src/com/darshan/addressbook/ContactsModule.java
@@ -9,4 +9,68 @@ public class ContactsModule {
     private String zipCode;
     private String phoneNo;
     private String email;
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
